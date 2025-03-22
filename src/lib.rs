1839d3a0c1ac5d9c0bcb0d858fb8cbfe3b9a77c7
Leviathan <levi.mcgomes@gmail.com>
2025-03-22 18:48:24 +0000

    feat: add rough model of packet structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,73 @@
+#![cfg_attr(not(test), no_std)]
 
+/// Data contained inside a packet
+///
+/// TODO: Make the payload generic
+pub struct Payload(u128);
+
+/// The ID of a device onboard the CanSat, as specified by the protocol
+///
+/// TODO: Autogenerate the enum variants from the protocol mapping
+pub enum DeviceId {}
+
+/// Time in nanoseconds since the Unix epoch
+pub struct Timestamp(u64);
+
+/// A packet containing metadata and a payload
+struct InternalPacket {
+    version: u8,
+    length: u8,
+    device_id: DeviceId,
+    timestamp: Timestamp,
+    payload: Payload,
+}
+
+/// A telemetry packet
+pub struct TmPacket(InternalPacket);
+
+impl TmPacket {
+    /// Create a new telemetry packet
+    pub fn new(
+        version: u8,
+        length: u8,
+        device_id: DeviceId,
+        timestamp: Timestamp,
+        payload: Payload,
+    ) -> Self {
+        TmPacket(InternalPacket {
+            version,
+            length,
+            device_id,
+            timestamp,
+            payload,
+        })
+    }
+}
+
+/// A telecommand packet
+pub struct TcPacket(InternalPacket);
+
+impl TcPacket {
+    /// Create a new telecommand packet
+    pub fn new(
+        version: u8,
+        length: u8,
+        device_id: DeviceId,
+        timestamp: Timestamp,
+        payload: Payload,
+    ) -> Self {
+        TcPacket(InternalPacket {
+            version,
+            length,
+            device_id,
+            timestamp,
+            payload,
+        })
+    }
+}
+
+/// A packet is either a telemetry packet or a telecommand packet
+pub enum Packet {
+    TmPacket(TmPacket),
+    TcPacket(TcPacket),
+}
